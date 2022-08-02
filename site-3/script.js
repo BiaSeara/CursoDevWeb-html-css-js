a017f5214d77a898a124d00e266b5339212e1f47
Bianca Seara <b.seara2601@gmail.com>
2022-08-02 19:17:08 -0300

    introduzindo js a pag fale conosco

--- site-3/script.js
+++ site-3/script.js
@@ -1,3 +1,5 @@
+/* APRESENTAÇÃO MODAL | PÁGINA INICIAL */
+
 // o modal irá mostrar depois de 5s.
 setTimeout (MostrarModal, 5000);
 function MostrarModal () {
@@ -14,26 +16,89 @@ function MostrarModal () {
     }
 }
 
+/* VALIDAÇÃO MODAL | PÁGINA INICIAL */
+
 //se o input foi preenchido, ou seja, diferente de undefined
 if (document.forms["modal-form"] != undefined) {
     var form = document.forms["modal-form"];
 
     // adicionar evento de envio.
     form.addEventListener("submit", validarForm);
+
+    form.email.addEventListener("keypress" /*key+press*/, function () {
+        form.email.className = ""; //aqui remove a classe de nao-valido do form, quando começar a digitar.
+
+        document.querySelector("span.nao-valido").style.display = "none";
+    })
 }
 
 function validarForm (evento) {
     // validação do form.
     var form = document.forms["modal-form"];
 
-    var valorEmail = form.email.value; //valor = o que está escrito no input.
+    //o que está escrito no input.
+    var inputEmail = form.email;
+    var valorEmail = form.email.value; 
 
     var posicaoArroba = valorEmail.indexOf("@"); //indexOf está buscando uma string com o valor @, busacando a posição.
     
-    if (valorEmail != "" /*string vazia*/ && valorEmail.indexOf("@") > 3 /*a posição do arroba é maior que 3*/ && valorEmail.lastIndexOf(".") > posicaoArroba /*a última ocorrência da ponto está depois do arroba*/) {
-        alert ("E-mail válido.");
-    } else {
-        alert ("E-mail inválido.");
+    if (!ValidarEmail (valorEmail)) {
+        inputEmail.className = "nao-valido"; //aqui adicionei a classe nao-valido ao input.
+
+        document.querySelector("span.nao-valido").style.display = "block"; //mostrará o span quando o email não for válido.
+
         evento.preventDefault(); //isso evitará que, caso seja um email inválido, após a aparição do span, envie para uma outra pagina. 
+    }
+}
+
+/* VALIDAÇÃO FORM | PÁGINA FALE CONOSCO */
+if (document.forms["form-cont"] != undefined) {
+    var form = document.forms["form-cont"];
+
+    form.addEventListener("submit", function (evento) {
+        var formValido = true;
+
+        if (!NaoVazio(form.nome.value)) {
+            form.nome.className = "nao-valido";
+            formValido = false;
+        }
+
+        if (!NaoVazio(form.telefone.value)) {
+            form.telefone.className = "nao-valido";
+            formValido = false;
+        }
+
+        if (!ValidarEmail(form.email.value)) {
+            form.email.className = "nao-valido";
+            formValido = false;
+        }
+
+        if (!NaoVazio(form.mensagem.value)) {
+            form.mensagem.className = "nao-valido";
+            formValido = false;
+        }
+
+
+        if (!formValido) {
+            evento.preventDefault();
+        }
+    });
+}
+
+
+/* FUNÇÕES */
+function ValidarEmail (valorEmail) {
+    if (valorEmail != "" /*string vazia*/ && valorEmail.indexOf("@") > 3 /*a posição do arroba é maior que 3*/ && valorEmail.lastIndexOf(".") > posicaoArroba /*a última ocorrência da ponto está depois do arroba*/) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function NaoVazio (texto) {
+    if (texto.trim().length > 0) {
+        return true; // trim = remove os espaçoes em branco.
+    } else {
+        return false;
     }
 }
